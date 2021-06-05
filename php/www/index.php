fb8e9f096603c81f18bda530a2ff2c1c30861a05
leeroy2342 <leeroy2342@gmail.com>
2021-06-05 17:03:28 +0300

    Third commit

--- php/www/index.php
+++ php/www/index.php
@@ -1,6 +1,9 @@
 <?php
-$link = mysqli_connect($MYSQL_NAME, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE, $EXTERNAL_SQL_PORT)
-or die("Error " . mysqli_error($link));
-echo "connected successfully";
+require_once 'connection.php';
+$link = mysqli_connect($host,$user,$password,$database);
+if($link)
+echo 'Соединение установлено';
+else
+die('Ошибка подключения');
 mysqli_close($link);
 ?>
