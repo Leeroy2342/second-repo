a61272ea9609f02f1cf26f690dbfe8e936771c75
leeroy2342 <leeroy2342@gmail.com>
2021-06-19 17:19:34 +0300

    second attempt

--- php/www/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = getenv('MYSQL_NAME');
-$user = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
-$database = getenv('MYSQL_DATABASE');
-$port = getenv('EXTERNAL_SQL_PORT');
-?>
