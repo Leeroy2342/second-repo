23a73bfd0097ab27a2bed66122abfb4a6ece168f
leeroy2342 <leeroy2342@gmail.com>
2021-06-20 12:31:58 +0300

    final ver

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
 FROM php:7.4-apache
-RUN docker-php-ext-install pdo
+RUN docker-php-ext-install mysqli
 COPY ./index.php /var/www/html/index.php
 COPY ./ports.conf /etc/apache2/ports.conf
--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 $db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+echo $db;
+$link=mysqli_connect($db);
+if ($link)
+echo 'Успешно';
+else die('Ошибка');
+mysqli_close($link);
 ?>
