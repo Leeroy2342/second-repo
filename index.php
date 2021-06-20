7be41adc02a8762921427871c612b934bf88b999
leeroy2342 <leeroy2342@gmail.com>
2021-06-20 12:55:50 +0300

    final ver

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-$url = parse_url(getenv("DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-$link = mysqli_connect($server, $username, $password, $db);
+$link = mysqli_connect($url);
 
 if ($link)
 echo 'Успешно';
