7152ace2600dc057a6b300912037974e09cd108f
leeroy2342 <leeroy2342@gmail.com>
2021-07-27 23:09:12 +0300

    Changed image source

--- php/www/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'connection.php';
-$link = mysqli_connect($host,$user,$password,$database);
-if($link)
-echo 'Соединение установлено';
-else
-die('Ошибка подключения');
-mysqli_close($link);
-?>
